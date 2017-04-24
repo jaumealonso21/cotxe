ef91e8e99fb7c31855b9c1b8aaed2c6dccebb357
jaumealonso21 <jaumealonso@hotmail.com>
2017-04-24 19:39:00 +0200

    Versió 2.1 Acaba

--- public_html/js/enlazador_old.js
+++ public_html/js/enlazador_old.js
@@ -8,6 +8,52 @@ $(function () {
     botoRuta = $('#botoRuta').on("click", marcar);
     botoRuta = $('#botoRuta').on("tap", marcar);
 
+    function enlazarMarcador(e) {
+
+        // muestra ruta entre marcas anteriores y actuales
+        map.drawRoute({
+            origin: [lat, lng], // origen en coordenadas anteriores
+            // destino en coordenadas del click o toque actual
+            destination: [e.latLng.lat(), e.latLng.lng()],
+            travelMode: 'driving',
+            strokeColor: '#000000',
+            strokeOpacity: 0.6,
+            strokeWeight: 5
+        });
+
+        lat = e.latLng.lat();   // guarda coords para marca siguiente
+        lng = e.latLng.lng();
+
+        map.addMarker({lat: lat, lng: lng});  // pone marcador en mapa
+    }
+    ;
+
+    function geolocalizar() {
+        GMaps.geolocate({
+            success: function (position) {
+                lat = position.coords.latitude;  // guarda coords en lat y lng
+                lng = position.coords.longitude;
+
+                map = new GMaps({// muestra mapa centrado en coords [lat, lng]
+                    el: '#map',
+                    lat: lat,
+                    lng: lng,
+                    height: "800px",
+                    width: "400px",
+                    click: enlazarMarcador,
+                    tap: enlazarMarcador
+                });
+                map.addMarker({lat: lat, lng: lng});  // marcador en [lat, lng]
+            },
+            error: function (error) {
+                alert('Geolocalización falla: ' + error.message);
+            },
+            not_supported: function () {
+                alert("Su navegador no soporta geolocalización");
+            }
+        });
+    }
+    ;
     function enlazar(e) {
         GMaps.geolocate({
             success: function (position) {
@@ -54,7 +100,7 @@ $(function () {
                     el: '#map',
                     lat: lat,
                     lng: lng,
-                    height: "300px",
+                    height: "800px",
                     width: "400px"
                 });
                 map.addMarker({lat: lat, lng: lng});  // marcador en [lat, lng]
