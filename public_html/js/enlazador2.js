15f7c9eb461afa24ac0a177ed8130f54e4dfa960
jaumealonso21 <jaumealonso@hotmail.com>
2017-04-28 19:38:39 +0200

    versió 5.1

--- public_html/js/enlazador2.js
+++ public_html/js/enlazador2.js
@@ -1,39 +1,45 @@
-var map, lat, lng, latRuta, lngRuta;
-var contenidor, botoCaptura, botoRuta;
+/* global GMaps */
 
+var map, lat, lng, latRuta, lngRuta;
+var contenidor, botoCaptura, botoRuta, menus;
+var host = pais = edif = [];
 $(function () {
-    contenidor = $('#contenidor');//De sortida el mapa ocultat
+    contenidor = $('#contenidor'); //De sortida el mapa ocultat
     botoCaptura = $('#botoCaptura').on("click", mostrar);
     botoCaptura = $('#botoCaptura').on("tap", mostrar);
     botoRuta = $('#botoRuta').on("click", marcar);
     botoRuta = $('#botoRuta').on("tap", marcar);
+    menus = $('#menus');
+    function marcar(e) {
 
-    function enlazarMarcador(e) {
-
-        // muestra ruta entre marcas anteriores y actuales
-//        map.drawRoute({
-//            origin: [lat, lng], // origen en coordenadas anteriores
-//            // destino en coordenadas del click o toque actual
-//            destination: [e.latLng.lat(), e.latLng.lng()],
-//            travelMode: 'driving',
-//            strokeColor: '#000000',
-//            strokeOpacity: 0.6,
-//            strokeWeight: 5
-//        });
-
-        lat = e.latLng.lat();   // guarda coords para marca siguiente
+        lat = e.latLng.lat(); // guarda coords para marca siguiente
         lng = e.latLng.lng();
-
-        map.addMarker({lat: lat, lng: lng});  // pone marcador en mapa
+//        map.addMarker({
+//            lat: lat,
+//            lng: lng,
+//            click: function (e) {
+//                alert('You clicked in this marker');
+//            }
+//        });  // pone marcador en mapa
+        map.addMarker({
+            lat: lat,
+            lng: lng,
+            infoWindow: {
+                content: '<span id=host> Hosteleria ||</span>\n\
+                                          <span id=pais> Paisatge natural ||</span>\n\
+                                          <span id=edif> Edificació d’interès ||</span>\n\
+                                          <span id=totes> Totes </span>'
+            }
+        });
+        menus.show();
     }
     ;
-
     function enlazar(e) {
         GMaps.geolocate({
             success: function (position) {
                 //latRuta = position.coords.latitude;  // guarda coords en lat y lng
                 //lngRuta = position.coords.longitude;
-                latRuta = position.coords.latitude + 0.001;  // proves
+                latRuta = position.coords.latitude + 0.001; // proves
                 lngRuta = position.coords.longitude + 0.001;
                 // muestra ruta entre marcas anteriores y actuales
                 map.drawRoute({
@@ -46,12 +52,11 @@ $(function () {
                     strokeOpacity: 0.6,
                     strokeWeight: 5
                 });
-
                 //lat = e.latLng.lat();   // guarda coords para marca siguiente
                 //lng = e.latLng.lng();
 
-                map.addMarker({lat: lat, lng: lng});  // pone marcador en mapa
-                map.addMarker({lat: latRuta, lng: lngRuta});  // pone marcador en mapa
+                map.addMarker({lat: lat, lng: lng}); // pone marcador en mapa
+                map.addMarker({lat: latRuta, lng: lngRuta}); // pone marcador en mapa
             },
             error: function (error) {
                 alert('Geolocalización falla: ' + error.message);
@@ -60,26 +65,23 @@ $(function () {
                 alert("Su navegador no soporta geolocalización");
             }
         });
-
     }
     ;
-
     function geoloc() {
         GMaps.geolocate({
             success: function (position) {
-                lat = position.coords.latitude;  // guarda coords en lat y lng
+                lat = position.coords.latitude; // guarda coords en lat y lng
                 lng = position.coords.longitude;
-
                 map = new GMaps({// muestra mapa centrado en coords [lat, lng]
                     el: '#map',
                     lat: lat,
                     lng: lng,
                     height: "600px",
                     width: "400px",
-                    click: enlazarMarcador,
-                    tap: enlazarMarcador
+                    click: marcar,
+                    tap: marcar
                 });
-                map.addMarker({lat: lat, lng: lng});  // marcador en [lat, lng]
+                //map.addMarker({lat: lat, lng: lng});  // marcador en [lat, lng]
             },
             error: function (error) {
                 alert('Geolocalización falla: ' + error.message);
@@ -102,7 +104,7 @@ $(function () {
         //geolocalizar();
     }
     //geolocalizar();
+
     geoloc();
-    
 });
 
